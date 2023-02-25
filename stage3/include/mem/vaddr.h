31047c7be5030df6c55db22fdb5a45006c95c886
Kieran Weaver <weaver2@student.ubc.ca>
2023-02-25 15:09:18 -0800

    Improve pt_ functions

--- stage3/include/mem/vaddr.h
+++ stage3/include/mem/vaddr.h
@@ -17,6 +17,7 @@ struct __attribute__((aligned(PAGE_SIZE))) page {
 
 #define PT_PAGE_MASK  0xFFFFF000
 #define PT_FLAGS_MASK 0x00000FFF
+#define PT_INVALID_PAGE 0xFFFFFFFF
 
 enum PT {          // Set           / Unset
 	PRESENT = 1,   // Present       / not present
@@ -31,7 +32,7 @@ enum PT {          // Set           / Unset
 //  Types of page, sets AVL bits
 	RAM_FREE = 0,
 	MMAP_PRESENT = 512,
-	MMAP_NOT_PRESENT = 1024,
+	MMAP_NOT_PRESENT = (512 * 2),
 	RAM_PRESENT = (512 * 3),
 	RAM_ALLOC_ON_WRITE = (512 * 4),
 	RAM_SWAP = (512 * 5),
@@ -44,10 +45,12 @@ paddr_t  get_paddr(vaddr_t vaddr);
 uint32_t get_flags(vaddr_t vaddr);
 // Map a single virtual page
 paddr_t  pt_map(paddr_t paddr, vaddr_t vaddr, uint32_t flags);
+// Map a page directory
+paddr_t  pd_map(paddr_t paddr, vaddr_t vaddr, uint32_t flags);
 // Unmap a single virtual page
-uint32_t pt_unmap(vaddr_t vaddr);
+paddr_t  pt_unmap(vaddr_t vaddr);
 // Replace virtual page's flags with new flags
-void     vmark(vaddr_t vaddr, uint32_t flags);
+paddr_t  vmark(vaddr_t vaddr, uint32_t flags);
 
 EXTERN_C_END
 
--- stage3/mem/vaddr.cpp
+++ stage3/mem/vaddr.cpp
@@ -10,7 +10,7 @@ paddr_t get_paddr(vaddr_t vaddr) {
 	uint32_t* pt = reinterpret_cast<uint32_t*>(0xFFC00000) + (0x400 * pdi);
 
 	if (((pd[pdi] & PRESENT) == 0) || ((pt[pti] & PRESENT) == 0)) {
-		return 0;
+		return PT_INVALID_PAGE;
 	}
  
 	return (pt[pti] & ~0xFFF) + (reinterpret_cast<uint32_t>(vaddr) & 0xFFF);
@@ -25,17 +25,28 @@ paddr_t pt_map(paddr_t paddr, vaddr_t vaddr, uint32_t flags) {
 	vaddr = reinterpret_cast<vaddr_t>(reinterpret_cast<uint32_t>(vaddr) & 0xFFFFF000);
 	
 	if ((pd[pdi] & PRESENT) == 0) { // Not present
-		return 0xFFFFFFFF;
+		return PT_INVALID_PAGE;
 	}
 
 	tmp = pagetable[pti];
-	pagetable[pti] = paddr | (flags & 0xFFF) | 0x01;
+	pagetable[pti] = paddr | (flags & PT_FLAGS_MASK) | PRESENT;
 	invlpg(reinterpret_cast<void*>(vaddr));
 	
 	return tmp;
 }
 
-uint32_t pt_unmap(vaddr_t vaddr) {
+paddr_t pd_map(paddr_t paddr, vaddr_t vaddr, uint32_t flags) {
+	uint32_t pdi = (reinterpret_cast<uint32_t>(vaddr) >> 22);
+	paddr_t old;
+
+	old = pd[pdi];
+	pd[pdi] = paddr | (flags & PT_FLAGS_MASK) | PRESENT;
+	invlpg(pd + pdi);
+
+	return old;
+}
+
+paddr_t pt_unmap(vaddr_t vaddr) {
 	uint32_t  pdi = (reinterpret_cast<uint32_t>(vaddr) >> 22);
 	uint32_t  pti = (reinterpret_cast<uint32_t>(vaddr) >> 12) & 0x3FF;
 	uint32_t* pagetable = ((uint32_t*)0xFFC00000) + (0x400 * pdi);
@@ -55,18 +66,26 @@ uint32_t get_flags(vaddr_t vaddr) {
 	const uint32_t  pti = (reinterpret_cast<uint32_t>(vaddr) >> 12) & 0x3FF;
 	const uint32_t* pt = reinterpret_cast<const uint32_t*>(0xFFC00000) + (0x400 * pdi);
 
+	if ((pd[pdi] & PRESENT) == 0) { // Not present
+		return PT_INVALID_PAGE;
+	}
+
 	return pt[pti] & 0xE00;
 }
 
-void vmark(vaddr_t vaddr, uint32_t flags) {
+paddr_t vmark(vaddr_t vaddr, uint32_t flags) {
 	uint32_t  pdi = (reinterpret_cast<uint32_t>(vaddr) >> 22);
 	uint32_t  pti = (reinterpret_cast<uint32_t>(vaddr) >> 12) & 0x3FF;
 	uint32_t* pagetable = ((uint32_t*)0xFFC00000) + (0x400 * pdi);
 	uint32_t  paddr = 0;
 
-	if ((pd[pdi] & PRESENT) != 0) {
-		paddr = pagetable[pti] & PT_PAGE_MASK;
-		pagetable[pti] = paddr | flags;
-		invlpg(reinterpret_cast<void*>(vaddr));
+	if ((pd[pdi] & PRESENT) == 0) {
+		return PT_INVALID_PAGE;
 	}
+	
+	paddr = pagetable[pti];
+	pagetable[pti] = (paddr & PT_PAGE_MASK) | flags;
+	invlpg(reinterpret_cast<void*>(vaddr));
+	
+	return paddr;
 }
